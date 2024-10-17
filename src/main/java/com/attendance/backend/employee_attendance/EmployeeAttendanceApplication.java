b510bd445d0492b5872d6cccc92032a648aab14a
Krushna Bangar <71035359+krushnabangar546@users.noreply.github.com>
2024-10-17 12:27:00 +0530

    attendance app backend push commit to git

---
+++ src/main/java/com/attendance/backend/employee_attendance/EmployeeAttendanceApplication.java
@@ -0,0 +1,13 @@
+package com.attendance.backend.employee_attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAttendanceApplication.class, args);
+	}
+
+}
